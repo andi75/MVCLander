dab0c1a8feed4a779a854eddf2401201990a4522
andi75 <umbach@gmail.com>
2017-04-18 21:30:41 +0200

    added JLabels to view

--- src/DataView.java
+++ src/DataView.java
@@ -1,10 +1,13 @@
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class DataView extends JPanel {
 	String labels[];
 	String data[];
+	
+	JLabel labelItems[];
 	
 	DataView(int nElements)
 	{
@@ -12,5 +15,22 @@ public class DataView extends JPanel {
 		data = new String[nElements];
 		
 		setLayout(new GridLayout(nElements, 2));
+		labelItems = new JLabel[2 * nElements];
+		for(int i = 0; i < nElements; i++)
+		{
+			labelItems[2 * i + 0] = new JLabel("");
+			labelItems[2 * i + 1] = new JLabel("");
+			add(labelItems[2 * i + 0]);
+			add(labelItems[2 * i + 1]);
+		}
+	}
+	
+	public void updateView()
+	{
+		for(int i = 0; i < labels.length; i++)
+		{
+			labelItems[2 * i + 0].setText(labels[i]);
+			labelItems[2 * i + 1].setText(data[i]);
+		}
 	}
 }
